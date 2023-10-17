e64a1c97cfc159f89f785513d05acff5b125919e
= <=>
2023-10-17 15:22:53 +0300

    gffggf

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = "3000";
 app.get("/as", function (req, res) {
   res.end("Hello students!");
-  console.log(res);
+});
+app.post("/as", function (req, res) {
+  console.log(req.body);
 });
 app.listen(port, () => {
   console.log("listening on port:" + port);
