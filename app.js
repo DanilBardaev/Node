fe620ec5f20c0f5b9351d0fc0efbd5d8ea73cd66
= <=>
2023-10-17 15:00:06 +0300

    terter

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-const path = require("path");
-// console.log("Название файла: ", path.basename(__filename));
-// console.log("Название каталога: ", path.dirname(__filename));
-// console.log("Расширение: ", path.extname(__filename));
-const fs = require("fs");
-// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log("Папка создана");
-// });
-const filePath = path.join(__dirname, "tmp", " 2 txt");
-console.log(filePath);
-fs.writeFile(filePath, "\n Something is wrong in the file", function (err) {
-  if (err) {
-    console.error(err);
-  }
-  console.log("Папка создана");
+import express from "express";
+const app = express();
+const port = "3000";
+app.get("/as", function (req, res) {
+  res.end("Hello students!");
+  console.log(res);
 });
-fs.appendFile(
-  filePath,
-  "\n Something went wrong with the file",
-  function (err) {
-    if (err) {
-      console.error(err);
-    }
-    console.log("Папка создана");
-  }
-);
-fs.readFile(filePath, (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data);
+app.listen(port, () => {
+  console.log("listening on port:" + port);
 });
