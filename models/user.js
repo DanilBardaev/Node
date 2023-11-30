e7fc40a93b17891136a69e0b5b4d5b846d100711
= <=>
2023-11-30 15:01:51 +0300

    commit master

--- models/user.js
+++ models/user.js
@@ -1,13 +1,42 @@
 const sqlite3 = require("sqlite").verbose();
 const db = new sqlite3.Database("test.sqlite");
+const bcrypt = require("bcrypt");
 
 const sql =
-  "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, password TEXT NOT NULL, age INTEGER NOT NULL)";
+  "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, password TEXT NOT NULL, age INTEGER NOT NULL, name TEXT NOT NULL)";
 
 db.run(sql);
 
 class User {
   constructor() {}
-  static create() {}
-  static findByEmail(email) {}
+  static async create(dataform, cb) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(dataform.password, salt);
+      const sql1 = "INSERT INTO user (id, password, age) VALUES (?,?, ?)";
+      db.run(sql1, dataform.email, hash, dataform.age, cb);
+    } catch (err) {
+      if (err) return next(err);
+    }
+  }
+
+  static findByEmail(email, cb) {
+    db.get("SELECT * FROM users WHERE email = ?", email, cb);
+  }
+
+  static authentificate(dataform, cb) {
+    User.findByEmail(dataform.email, cb),
+      (err, user) => {
+        if (err) {
+          return cb(err);
+        }
+        if (!user) {
+          return cb();
+        }
+        const result = bcrypt.compare(dataform.password, user.password);
+        if (result) {
+          return cb(user); // TODO: check
+        }
+      };
+  }
 }
