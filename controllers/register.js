db2a51b31cabe2d4cddc41b3c1afeef15abc1166
= <=>
2023-11-30 15:24:45 +0300

    ig7

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,9 @@
+const User = require("../models/user");
 exports.form = (req, res) => {
   res.render("register", {});
+};
+
+exports.submit = (req, res, next) => {
+  // const email = req.body.user.email;
+  if ("!Игорь есть в базе данных?") User.create(req.body.user, cb);
 };
